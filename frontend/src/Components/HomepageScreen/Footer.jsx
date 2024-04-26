46d182a2995465cb937321104d1bd0e9abc35037
Nishant Ojha <ojhasumit0428@gmail.com>
2024-04-26 10:12:04 +0530

    first commit

---
+++ frontend/src/Components/HomepageScreen/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+    const date = new Date();
+    const year = date.getFullYear();
+  return (
+    <> 
+        <div class="wave-container wave">
+            <p>© {year}, Developed & Designed with ❤️ by <a className='footer_faizan' target='_faizan' href='https://github.com/nishant11345'>Nishant Ojha</a></p>
+        </div>
+    </>
+  )
+}
+
+export default Footer
